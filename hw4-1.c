f362147c38ec8d99c46ae04ca705ec9fee07598f
112310219 <146065173+112310219@users.noreply.github.com>
2023-10-30 19:38:37 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main()
+
+{
+int input, count;
+int x,ar;
+int n ,c = 2;
+
+scanf("%d" , &ar);
+
+int a[ar];
+
+for (int i = 0; i < ar;i++ ){
+    scanf("%d", &a[i]);
+}
+for(int p = 0;p < ar;p++ ){
+    int input = a[p];
+
+    for ( c = 2 ; c < input ; c++ ){
+    if ( input%c == 0 ){
+    printf("NO ");
+    break;
+    }
+}
+    if ( c == input )
+    printf("YES ");
+
+}
+return 0;
+}
 
