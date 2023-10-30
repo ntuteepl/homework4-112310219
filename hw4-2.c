46e1ef903494c507f970d318812447ce8f506c27
112310219 <146065173+112310219@users.noreply.github.com>
2023-10-30 19:38:58 +0800

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+int count,input;
+int x;
+int ar;
+
+scanf("%d",&ar);
+
+
+int database [6][3] ={
+    {123 , 456 , 9000},
+    {456 , 789 , 5000},
+    {789 , 888 , 6000},
+    {336 , 558 , 10000},
+    {775 , 666 , 12000},
+    {566 , 221 , 7000}};
+
+
+
+int account , password;
+for (int y = 0; y < ar; y++){
+    scanf("%d%d", &account , &password);
+    bool used = false;
+    for (int j = 0; j < 6 ; j++){
+        if (account == database[j][0] && password == database[j][1]){
+            printf("%d\n", database[j][2]);
+            used = true ;
+        }
+    }
+    if (!used) printf("error\n");
+}
+
+
+return 0;
+
+}
 
